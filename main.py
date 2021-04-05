d03a12df1d000c53608681f3c59c1a3ea220d2cf
DiazRock <corolariodiaz@gmail.com>
2021-04-05 16:29:44 -0400

    Added some clients creation

---
+++ main.py
@@ -0,0 +1,18 @@
+
+clients = 'pablo,ricardo,'
+
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+
+
+def _add_comma():
+    global clients
+    clients +=','
+
+
+
+if __name__ == '__main__':
+    create_client('David')
+    print(clients)
