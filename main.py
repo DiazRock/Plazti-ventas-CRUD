ae065a472228ac38b4eed81b9efa6af9d53272e2
DiazRock <corolariodiaz@gmail.com>
2021-04-06 11:59:22 -0400

    First work

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 clients = 'pablo,ricardo,'
 
 
@@ -12,7 +11,21 @@ def _add_comma():
     clients +=','
 
 
+def list_clients():
+    global clients
+    
+    print(clients)
+
+
+def _print_wellcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+    
 
 if __name__ == '__main__':
+    list_clients()
     create_client('David')
     print(clients)
